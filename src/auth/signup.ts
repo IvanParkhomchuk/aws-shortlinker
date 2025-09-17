dcb47f8c58d0d066a8c446a02adf09a93be2ff98
Ivan Parkhomchuk <workaccbyivan@gmail.com>
2025-09-17 22:44:26 +0300

    signup with DynamoDB
    - Added saving new user to database
    - Added check whether user already exists

--- src/auth/signup.ts
+++ src/auth/signup.ts
@@ -1,5 +1,6 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, PutItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
 import bcrypt from "bcryptjs";
+import * as uuid from 'uuid';
 
 const client = new DynamoDBClient();
 
@@ -7,9 +8,22 @@ export const handler = async(event: any) => {
     try {
         const requestBody = JSON.parse(event.body);
         const { username, password } = requestBody;
-
         const saltRounds = 10;
         
+        const scanParams = {
+            TableName: process.env.USERS_TABLE,
+            ProjectionExpression: "username",
+        };
+
+        const data = await client.send(new ScanCommand(scanParams));
+        const existingUsername = data.Items?.map(item => item.username?.S);
+
+        if (existingUsername?.includes(username)) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Username already exists" }),
+            }
+        }
         if (!username || typeof username !== "string") {
             return {
                 statusCode: 400,
@@ -24,10 +38,20 @@ export const handler = async(event: any) => {
         }
 
         const hashedPassword = await bcrypt.hash(password, saltRounds); 
+        const putParams = {
+            TableName: process.env.USERS_TABLE,
+            Item: {
+                userId: { S: uuid.v4() },
+                username: { S: username },
+                password: { S: hashedPassword }
+            }
+        };
+
+        await client.send(new PutItemCommand(putParams));
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: "Signup received", username, password }),
+            body: JSON.stringify({ message: "Signup received successfuly", username }),
         };
     } catch (err) {
         console.error("Signup error:", err);
